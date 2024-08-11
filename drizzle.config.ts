17ff5c65d0c3f1510813814a50fd2072327a8191
Robert Grayson <robert.samuel.grayson@gmail.com>
2024-08-11 23:54:12 +0900

    Add courses page

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,21 +1,9 @@
-// import "dotenv/config"
-// import type { Config } from "drizzle-kit"
-
-// export default {
-//   schema: "./db/schema.ts",
-//   out: "./drizzle",
-//   driver: "pg",
-//   dbCredentials: {
-//     connectionString: process.env.DATABASE_URL!,
-//   },
-// } satisfies Config
-
 import "dotenv/config";
 import {defineConfig} from "drizzle-kit"
 
 export default defineConfig({
     dialect: "postgresql",
-    schema: "./database/schema.ts",
+    schema: "./db/schema.ts",
     dbCredentials: {
         url: process.env.DATABASE_URL!,
 
