c0a0c934040d7ab33fa1bc7cb8d124dbbc0ab6df
Robert Grayson <robert.samuel.grayson@gmail.com>
2024-08-15 18:05:50 +0900

    Complete units set up

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,17 +2,20 @@ import { redirect } from "next/navigation"
 import { StickyWrapper } from "@/components/sticky-wrapper"
 import { FeedWrapper } from "@/components/feed-wrapper"
 import { UserProgress } from "@/components/user-progress"
-import { getUserProgress } from "@/db/queries"
+import { getUnits, getUserProgress } from "@/db/queries"
 
 import { Header } from "./header"
 
 const LearnPage = async () => {
   const userProgressData = getUserProgress()
+  const unitsData = getUnits()
 
   const [
-    userProgress
+    userProgress,
+    units
   ] = await Promise.all([
-    userProgressData
+    userProgressData,
+    unitsData
   ])
 
   if(!userProgress || !userProgress.activeCourse) {
@@ -31,6 +34,11 @@ const LearnPage = async () => {
       </StickyWrapper>
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            {JSON.stringify(unit)}
+          </div>
+        ))}
       </FeedWrapper>
     </div>
   )
