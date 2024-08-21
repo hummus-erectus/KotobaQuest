efdbc4c6cc24a15b846c9c2a2851109cddb66ac5
Robert Grayson <robert.samuel.grayson@gmail.com>
2024-08-21 14:37:39 +0900

    Add Stripe routing

--- app/(main)/shop/items.tsx
+++ app/(main)/shop/items.tsx
@@ -101,9 +101,9 @@ export const Items = ({
         </div>
         <Button
           onClick={onUpgrade}
-          disabled={pending || hasActiveSubscription}
+          disabled={pending}
         >
-          {hasActiveSubscription ? "active" : "upgrade"}
+          {hasActiveSubscription ? "settings" : "upgrade"}
         </Button>
       </div>
     </ul>
