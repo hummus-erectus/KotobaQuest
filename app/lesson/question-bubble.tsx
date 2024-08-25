e06b1fc18685b19311f0a8d6b68d1d28eb4b6b7c
Robert Grayson <robert.samuel.grayson@gmail.com>
2024-08-25 18:54:26 +0900

    Add nes question bubble

--- app/lesson/question-bubble.tsx
+++ app/lesson/question-bubble.tsx
@@ -21,11 +21,9 @@ export const QuestionBubble = ({ question }: Props) => {
         width={40}
         className="block lg:hidden"
       />
-      <div className="relative py-2 px-4 border-2 rounded-xl text-sm font-dotgothic16 lg:text-base">
+
+      <div className="nes-balloon from-left text-sm font-dotgothic16 font-bold lg:text-base">
         {question}
-        <div
-          className="absolute -left-3 top-1/2 w-0 h-0 border-x-8 border-x-transparent border-t-8 transofrm -translate-y-1/2 rotate-90"
-        />
       </div>
     </div>
   )
