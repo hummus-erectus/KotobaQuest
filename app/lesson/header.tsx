425df8277d1abe81f472bceb25d71dbf436ae52b
Robert Grayson <robert.samuel.grayson@gmail.com>
2024-08-27 21:32:02 +0900

    Add styles to lesson header

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -18,12 +18,9 @@ export const Header = ({
 
   return (
     <header className="lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full" >
-      <X
-        onClick={open}
-        className="text-slate-500 hover:opacity-75 transition cursor-pointer"
-      />
-      <Progress value={percentage} />
-      <div className="text-rose-500 flex items-center font-bold">
+      <span onClick={open} className="font-pressStart2P text-slate-500 hover:opacity-75 transition cursor-pointer">X</span>
+      <progress value={percentage} max="100"className="nes-progress nes-progress-sm is-success"/>
+      <div className="text-rose-500 flex items-center font-pressStart2P font-bold">
         <Image
           src="/heart.svg"
           height={28}
