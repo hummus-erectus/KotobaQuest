de841407f40779d5d08b9fc15dc69d6821e124d7
Robert Grayson <robert.samuel.grayson@gmail.com>
2024-09-05 01:37:28 +0900

    Adjust quests

--- db/constants.ts
+++ db/constants.ts
@@ -3,22 +3,22 @@ export const MAXIMUM_HEARTS = 5
 
 export const quests = [
   {
-    title: "20 XP を獲得する",
-    value: 20,
-  },
-  {
-    title: "50 XP を獲得する",
-    value: 50,
-  },
-  {
-    title: "100 XP を獲得する",
-    value: 100,
+    title: "250 XP を獲得する",
+    value: 250,
   },
   {
     title: "500 XP を獲得する",
     value: 500,
-  },{
+  },
+  {
     title: "1000 XP を獲得する",
     value: 1000,
+  },
+  {
+    title: "2500 XP を獲得する",
+    value: 2500,
+  },{
+    title: "5000 XP を獲得する",
+    value: 5000,
   },
 ]
