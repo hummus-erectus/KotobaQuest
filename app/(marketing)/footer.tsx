f1961401d80a017dc56c7c04eeb9b1d2785d7399
Robert Grayson <robert.samuel.grayson@gmail.com>
2024-09-06 12:51:48 +0900

    Update splash screen footer with LinkedIn link

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,60 +1,13 @@
 import { Button } from "@/components/ui/button"
 import Image from "next/image"
+import Link from "next/link"
 
 export const Footer = () => {
+  const currentYear = new Date().getFullYear()
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      <div className="max-w-scrreen-lg mx-auto flex items-center justify-evenly h-full">
-        <Button size="lg" variant="ghost" className="-w">
-          <Image
-          src="/GB.svg"
-          alt="English"
-          height={32}
-          width={40}
-          className="mr-4 rounded-md"
-          />
-          English
-        </Button>
-        <Button size="lg" variant="ghost" className="-w">
-          <Image
-          src="/FR.svg"
-          alt="French"
-          height={32}
-          width={40}
-          className="mr-4 rounded-md"
-          />
-          French
-        </Button>
-        <Button size="lg" variant="ghost" className="-w">
-          <Image
-          src="/DE.svg"
-          alt="German"
-          height={32}
-          width={40}
-          className="mr-4 rounded-md"
-          />
-          German
-        </Button>
-        <Button size="lg" variant="ghost" className="-w">
-          <Image
-          src="/JP.svg"
-          alt="Japanese"
-          height={32}
-          width={40}
-          className="mr-4 rounded-md"
-          />
-          Japanese
-        </Button>
-        <Button size="lg" variant="ghost" className="-w">
-          <Image
-          src="/IL.svg"
-          alt="Hebrew"
-          height={32}
-          width={40}
-          className="mr-4 rounded-md"
-          />
-          Hebrew
-        </Button>
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <p className="font-pressStart2P text-neutral-600 mb-0">&copy; Copyright {currentYear} <Link href="https://www.linkedin.com/in/rob-grayson/">Robert Grayson</Link>. All rights reserved</p>
       </div>
     </footer>
   )
